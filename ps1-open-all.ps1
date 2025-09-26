06c40f4e3c9aa7d53dd0dc88db1335fdca894646
snowfl0wer <snowfl0wer@proton.me>
2025-09-27 01:19:47 +0300

    Add files via upload

---
+++ ps1-open-all.ps1
@@ -0,0 +1,12 @@
+﻿$folders = gci  -Directory
+$foldersAmnt = ($folders | measure).Count
+
+
+for ($i=0; $i -lt $foldersAmnt; $i++){
+    cd $folders[$i]
+    if (Test-Path Telegram.exe) {
+       .\Telegram.exe
+    }
+    cd ../
+}
+
