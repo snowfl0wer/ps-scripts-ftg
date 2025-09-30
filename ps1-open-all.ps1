f774764f99692b06265a7abbe3cdaa99806fd791
snowfl0wer <snowfl0wer@proton.me>
2025-10-01 00:42:22 +0300

    add scale to ps1

--- ps1-open-all.ps1
+++ ps1-open-all.ps1
@@ -1,12 +1,11 @@
-﻿$folders = gci  -Directory
+$folders = gci  -Directory
 $foldersAmnt = ($folders | measure).Count
-
+$scale = Read-Host -Prompt "Enter scale%"
 
 for ($i=0; $i -lt $foldersAmnt; $i++){
     cd $folders[$i]
     if (Test-Path Telegram.exe) {
-       .\Telegram.exe
+       .\Telegram.exe -scale $scale
     }
     cd ../
 }
-
